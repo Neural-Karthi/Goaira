0397797a6d01bb2ef6147f6ac28354be371cf9b8
M Karthikeyan <amariyappan.karthikeyan@gmail.com>
2025-05-12 10:55:07 +0530

    first commit 2

--- src/view/RegisterationPage.jsx
+++ src/view/RegisterationPage.jsx
@@ -8,6 +8,11 @@ import icon_3 from '../assets/images/Registration_Icon_3.svg'
 import icon_4 from '../assets/images/Registration_Icon_4.svg'
 import icon_5 from '../assets/images/Registration_Icon_5.svg'
 import Question_component from '../component/Question_component.jsx'
+import Goaira_customer_screen from '../assets/images/Goaira_customer_screen.svg'
+import Count_1 from '../assets/images/Count_1.svg'
+import Count_2 from '../assets/images/Count_2.svg'
+import Count_3 from '../assets/images/Count_3.svg'
+import Count_4 from '../assets/images/Count_4.svg'
 
 const RegisterationPage = () => {
   useEffect(() => {
@@ -29,7 +34,7 @@ const RegisterationPage = () => {
         </div>
       </div>
 
-      <div className="py-5 pt-10 px-5 md:w-[90%] lg:w-[80%] 2xl:w-[70%]  border-b border-[#b5bbb8] mr-auto ml-auto">
+      <div className="py-5 pt-18 px-5 md:w-[90%] lg:w-[80%] 2xl:w-[70%]  border-b border-[#b5bbb8] mr-auto ml-auto">
          <div className="">
              <h1 className="text-2xl md:text-3xl font-Plancsemibold text-black">Why choose Goaira?</h1>
              <h1 className="text-[#5E6562] text-[16px] md:text-xl py-2 font-PlancRegular">Drive when you want, earn as much as you need <br className="hidden md:block" />— Goaira fits your lifestyle</h1>
@@ -76,6 +81,65 @@ const RegisterationPage = () => {
                </div>
             </div>
          </div>
+      </div>
+      <div className="w-full bg-[#F1F3F6] flex items-center justify-center">
+        <div className="py-5 pt-10 px-5 md:px-0 md:w-[90%] lg:w-[80%] 2xl:w-[70%] mr-auto ml-auto">
+         <div className="flex flex-col gap-4 md:gap-8 py-5 md:py-10">
+            <div className="flex flex-col lg:flex-row gap-4 md:gap-8">
+              <div className="flex-1 flex flex-col gap-1.5 md:gap-3">
+                   <img src={Goaira_customer_screen} alt="" className="w-full h-full object-cover" />
+               </div>
+               <div className="flex-1 flex flex-col py-5 gap-1.5 md:gap-3">
+                    <div className="">
+                      <h1 className="text-2xl md:text-3xl font-Plancsemibold text-center md:text-left text-black">Register. Drive. Earn. Repeat.</h1>
+                      <h1 className="text-[#5E6562] text-[16px] md:text-xl py-2 font-PlancRegular text-center md:text-left">Reliable and easy to use, providing everything you need to drive, deliver, and earn on your own terms</h1>
+                    </div>
+                    <div className="flex flex-col gap-5 py-5 px-5 md:px-0">
+                        {/* Count - 1 */}
+                        <div className="flex flex-col md:flex-row gap-3 md:gap-5">
+                           <div className="w-[40px]">
+                              <img src={Count_1} alt="" className="w-8"/>
+                           </div>
+                            <div className="flex-1">
+                             <h1 className="text-xl  md:text-2xl font-Plancsemibold text-black">Accept a Ride Request</h1>
+                             <h1 className="text-[#5E6562] text-[16px] md:text-lg pt-1 font-PlancRegular 2xl:w-[80%]">The Goaira Driver app will automatically find passengers for you in your area.</h1>
+                           </div>
+                        </div>
+                        {/* Count - 2 */}
+                        <div className="flex flex-col md:flex-row gap-3 md:gap-5">
+                            <div className="w-[40px]">
+                              <img src={Count_2} alt="" className="w-8"/>
+                           </div>
+                           <div className="flex-1">
+                             <h1 className="text-xl  md:text-2xl font-Plancsemibold  text-black">Pick Up Your Passenger</h1>
+                             <h1 className="text-[#5E6562] text-[16px] md:text-lg pt-1 font-PlancRegular 2xl:w-[80%]">The app will guide you to your passenger’s location. Once there, pick them up.</h1>
+                           </div>
+                        </div>
+                        {/* Count - 3 */}
+                        <div className="flex flex-col md:flex-row gap-3 md:gap-5">
+                            <div className="w-[40px]">
+                              <img src={Count_3} alt="" className="w-8"/>
+                           </div>
+                           <div className="flex-1">
+                             <h1 className="text-xl  md:text-2xl font-Plancsemibold  text-black">Drive to Their Destination</h1>
+                             <h1 className="text-[#5E6562] text-[16px] md:text-lg pt-1 font-PlancRegular 2xl:w-[80%]">Navigate to their destination with the app’s directions, and swipe to end the ride when you reach it.</h1>
+                           </div>
+                        </div>
+                        {/* Count - 4 */}
+                        <div className="flex flex-col md:flex-row gap-3 md:gap-5">
+                           <div className="w-[40px]">
+                              <img src={Count_4} alt="" className="w-8"/>
+                           </div>
+                           <div className="flex-1">
+                             <h1 className="text-xl  md:text-2xl font-Plancsemibold text-black">Repeat to Earn More</h1>
+                             <h1 className="text-[#5E6562] text-[16px] md:text-lg pt-1 font-PlancRegular 2xl:w-[80%]">Keep driving and earning or go offline — you’re always in full control of your schedule.</h1>
+                           </div>
+                        </div>
+                    </div>
+               </div>
+            </div>
+         </div>
+      </div>
       </div>
       <Question_component />
     </div>
