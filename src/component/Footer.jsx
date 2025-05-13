5247f23df6f757f2c1a341e563e7fd54e03f6f58
M Karthikeyan <amariyappan.karthikeyan@gmail.com>
2025-05-13 14:47:35 +0530

    first commit 2

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,8 +5,9 @@ import Social_media_2 from '../assets/images/Social_media_2.svg'
 import Social_media_3 from '../assets/images/Social_media_3.svg'    
 import Social_media_4 from '../assets/images/Social_media_4.svg'    
 import Social_media_5 from '../assets/images/Social_media_5.svg'    
-
+import { useNavigate,Link } from 'react-router-dom';
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <div className="w-full flex flex-col text-white bg-black pb-12 md:pb-22">
         <div className="px-4 md:px-0 w-full md:w-[80%] lg:w-[75%] 2xl:w-[60%] flex flex-col gap-5 md:gap-7.5 mr-auto ml-auto">
@@ -17,7 +18,7 @@ const Footer = () => {
                   <div className="flex-1">
                       <h1 className="text-lg lg:text-2xl font-Plancsemibold pb-2 md:pb-4">Company</h1>
                       <div className="flex flex-col gap-1">
-                        <a href="/Aboutus" className="text-[14px] md:text-lg font-PlancRegular">Our offerings</a>
+                        <button  onClick={() => {navigate('/Aboutus');setTimeout(() => window.scrollTo(0, 0), 100);}} className="text-[14px] md:text-lg font-PlancRegular">Our offerings</button>
                         <h1 className="text-[14px] md:text-lg font-PlancRegular">Newsroom</h1>
                         <h1 className="text-[14px] md:text-lg font-PlancRegular">Investors</h1>
                         <h1 className="text-[14px] md:text-lg font-PlancRegular">Blog</h1>
